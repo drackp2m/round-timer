2aee328891a78941c7cd14672488fac5a799d339
Marc Jovaní González <12443324+drackp2m@users.noreply.github.com>
2025-01-04 12:36:53 +0000

    🐛 fix: routes with hash to work in github pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { APP_ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideZoneChangeDetection({ eventCoalescing: true }),
-		provideRouter(APP_ROUTES),
+		provideRouter(APP_ROUTES, withHashLocation()),
 		provideAnimations(),
 	],
 };
